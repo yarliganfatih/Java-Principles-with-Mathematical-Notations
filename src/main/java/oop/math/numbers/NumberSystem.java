474f2a203e56d8c6a5ff3f2256c0d563f9bdf43a
Fatih Yarlıgan <fatih-yarligan@outlook.com>
2024-06-05 19:01:09 +0300

    used OOP.encapsulation to validate attributes

--- src/main/java/oop/math/numbers/NumberSystem.java
+++ src/main/java/oop/math/numbers/NumberSystem.java
@@ -1,70 +1,107 @@
 package oop.math.numbers;
 
 public class NumberSystem {
-    int sign = +1;
-    int digits = 0;
-    int radix = 10;
+    private int sign = +1;
+    private int digits = 0;
+    private int radix = 10;
+
+    public int getSign() {
+        return this.sign;
+    }
+    public void setSign(int number) {
+        if (number >= 0) this.sign = +1;
+        else if (number < 0) this.sign = -1;
+    }
+    public void reverse() {
+        this.setSign(this.sign * -1);
+    }
+
+    public int getDigits() {
+        return this.digits;
+    }
+    public void setDigits(int digits) {
+        if (digits < 0)
+            throw new NumberFormatException("Digits cannot be negative.");
+        int _digits = digits;
+        while (_digits >= 1) {
+            if (_digits % 10 >= this.radix)
+                throw new NumberFormatException("Invalid digit (" + _digits % 10 + ") for this radix (" + this.radix + ").");
+            _digits /= 10;
+        }
+        this.digits = digits;
+        if (this.digits == 0)
+            this.setSign(0); // to avoid -0
+    }
+
+    public int getRadix() {
+        return this.radix;
+    }
+    public void setRadix(int radix) {
+        if (radix < 2)
+            throw new IllegalArgumentException("Radix cannot be less than 2.");
+        this.radix = radix;
+    }
 
     public NumberSystem() {
     }
     public NumberSystem(int number) {
-        this.sign = number < 0 ? -1 : +1;
-        this.digits = Math.abs(number);
+        this.setSign(number);
+        this.setDigits(Math.abs(number));
     }
     public NumberSystem(String numberStr, int radix) {
+        this.setRadix(radix); // priority for comparison
         int number = Integer.parseInt(numberStr); // TODO should be able to catch letters
-        this.sign = number < 0 ? -1 : +1;
-        this.digits = Math.abs(number); // TODO each digit must be compatible with radix
-        this.radix = radix; // TODO cannot be less than 2
+        this.setSign(number);
+        this.setDigits(Math.abs(number));
     }
 
     // TODO operation methods should be developed for different radixed numbers other than decimal
 
     public NumberSystem add(NumberSystem other) {
-        if (this.radix != other.radix)
+        if (this.getRadix() != other.getRadix())
             throw new IllegalArgumentException("Incompatible radix value.");
-        int decimal1 = this.sign * this.digits;
-        int decimal2 = other.sign * other.digits;
+        int decimal1 = this.getSign() * this.getDigits();
+        int decimal2 = other.getSign() * other.getDigits();
         int result = decimal1 + decimal2;
-        this.sign = result < 0 ? -1 : +1;
-        this.digits = Math.abs(result);
+        this.setSign(result);
+        this.setDigits(Math.abs(result));
         return this;
     }
 
     public NumberSystem subt(NumberSystem other) {
-        other.sign *= -1;
+        other.reverse();
         return this.add(other);
     }
 
     public NumberSystem mult(NumberSystem other) {
-        if (this.radix != other.radix)
+        if (this.getRadix() != other.getRadix())
             throw new IllegalArgumentException("Incompatible radix value.");
-        int decimal1 = this.sign * this.digits;
-        int decimal2 = other.sign * other.digits;
+        int decimal1 = this.getSign() * this.getDigits();
+        int decimal2 = other.getSign() * other.getDigits();
         int result = decimal1 * decimal2;
-        this.sign = result < 0 ? -1 : +1;
-        this.digits = Math.abs(result);
+        this.setSign(result);
+        this.setDigits(Math.abs(result));
         return this;
     }
 
     public NumberSystem divi(NumberSystem other) {
-        if (this.radix != other.radix)
+        if (this.getRadix() != other.getRadix())
             throw new IllegalArgumentException("Incompatible radix value.");
-        if (other.digits == 0)
+        if (other.getDigits() == 0)
             throw new IllegalArgumentException("Denominator cannot be zero.");
-        int decimal1 = this.sign * this.digits;
-        int decimal2 = other.sign * other.digits;
+        int decimal1 = this.getSign() * this.getDigits();
+        int decimal2 = other.getSign() * other.getDigits();
         int result = decimal1 / decimal2;
-        this.sign = result < 0 ? -1 : +1;
-        this.digits = Math.abs(result);
+        this.setSign(result);
+        this.setDigits(Math.abs(result));
         return this;
     }
 
     public String toString() {
-        String out = this.sign == -1 ? "-" : "";
-        out += Integer.toString(this.digits);
-        if (this.radix != 10)
-            out += "_" + this.radix;
+        String out = this.getSign() == -1 ? "-" : "";
+        out += Integer.toString(this.getDigits());
+        if (this.getRadix() != 10)
+            out += "_" + this.getRadix();
         return out;
     }
 }
--- src/test/java/oop/math/numbers/NumberSystemTest.java
+++ src/test/java/oop/math/numbers/NumberSystemTest.java
@@ -1,39 +1,91 @@
 package oop.math.numbers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
 public class NumberSystemTest {
+    @Test
+    public void testSetSign() {
+        NumberSystem ns = new NumberSystem(1);
+        ns.setSign(+1);
+        assertEquals(+1, ns.getSign());
+        ns.setSign(+2);
+        assertEquals(+1, ns.getSign());
+        ns.setSign(-1);
+        assertEquals(-1, ns.getSign());
+        ns.setSign(-2);
+        assertEquals(-1, ns.getSign());
+        ns.setSign(0);
+        assertEquals(+1, ns.getSign());
+
+        ns = new NumberSystem(-0);
+        assertEquals(+1, ns.getSign());
+
+        ns = new NumberSystem(1);
+        ns.reverse();
+        assertEquals(-1, ns.getSign());
+        ns.reverse();
+        assertEquals(+1, ns.getSign());
+    }
+
+    @Test
+    public void testSetRadix() {
+        NumberSystem ns = new NumberSystem();
+        ns.setRadix(8);
+        assertEquals(8, ns.getRadix());
+
+        assertThrows(IllegalArgumentException.class, () -> ns.setRadix(1));
+
+        assertThrows(IllegalArgumentException.class, () -> new NumberSystem("0", 1));
+    }
+
+    @Test
+    public void testSetDigits() {
+        NumberSystem ns = new NumberSystem("-12", 10);
+        ns.setDigits(123);
+        assertEquals("-123", ns.toString());
+
+        assertThrows(NumberFormatException.class, () -> ns.setDigits(-10));
+
+        assertThrows(NumberFormatException.class, () -> new NumberSystem("981", 8));
+
+        ns.setDigits(0);
+        assertEquals(+1, ns.getSign());
+    }
 
     @Test
     public void testDefaultConstructor() {
         NumberSystem ns = new NumberSystem();
-        assertEquals(10, ns.radix);
-        assertEquals(0, ns.digits);
+        assertEquals(10, ns.getRadix());
+        assertEquals(0, ns.getDigits());
         assertEquals("0", ns.toString());
     }
 
     @Test
     public void testConstructorWithNumber() {
         NumberSystem ns = new NumberSystem(123);
-        assertEquals(123, ns.digits);
-        assertEquals(10, ns.radix);
+        assertEquals(123, ns.getDigits());
+        assertEquals(10, ns.getRadix());
         assertEquals("123", ns.toString());
     }
 
     @Test
     public void testConstructorWithString() {
         NumberSystem ns = new NumberSystem("101", 2);
-        assertEquals(101, ns.digits);
-        assertEquals(+1, ns.sign);
-        assertEquals(2, ns.radix);
+        assertEquals(101, ns.getDigits());
+        assertEquals(+1, ns.getSign());
+        assertEquals(2, ns.getRadix());
         assertEquals("101_2", ns.toString());
 
         ns = new NumberSystem("-25", 10);
-        assertEquals(25, ns.digits);
-        assertEquals(-1, ns.sign);
-        assertEquals(10, ns.radix);
+        assertEquals(25, ns.getDigits());
+        assertEquals(-1, ns.getSign());
+        assertEquals(10, ns.getRadix());
         assertEquals("-25", ns.toString());
+
+        ns = new NumberSystem("-0", 10);
+        assertEquals(+1, ns.getSign());
     }
 }
